a912984cbcac13aafb88bf144227cd34a380f2eb
unknown <aaron.leyesa@gmail.com>
2018-08-07 12:07:05 -0700

    added blog post get request

--- blogRouter.js
+++ blogRouter.js
@@ -4,4 +4,12 @@ const router = express.Router();
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 
-const {BlogPost} = require('./models');
+const {BlogPosts} = require('./models');
+
+BlogPosts.create('My Introduction', 'Hi my name is Bobby', 'Aaron L.', '9/1/18');
+
+router.get('/', (requestAnimationFrame, res) => {
+  res.json(BlogPosts.get());
+});
+
+module.exports = router;
